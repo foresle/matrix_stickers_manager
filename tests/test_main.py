e267ddbe674ce7e612471326d292dc94d162a4c8
foresle <foresle@tutanota.com>
2023-02-02 12:16:47 +0200

    Updated tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,73 +1,125 @@
 import pytest
 import uuid
 from environs import Env
+import unittest
 
 from matrix_stickers_manager import MatrixStickersManager, MatrixStickersManagerError
 
 
-@pytest.fixture
-def manager() -> MatrixStickersManager:
-    return MatrixStickersManager(path_to_config='../config.yaml')
+class TestStringMethods(unittest.TestCase):
+    manager: MatrixStickersManager
+    env_vars: Env
+    test_created_pack: str
 
+    def setUp(self) -> None:
+        # Load env variables
+        env = Env()
+        env.read_env('test.env')
+        self.env_vars = env
 
-@pytest.fixture
-def env_variables() -> Env:
-    env = Env()
-    env.read_env('test.env')
-    return env
+        # Create manager
+        self.manager = MatrixStickersManager(path_to_config='../config.yaml')
+        self.test_created_pack = uuid.uuid4().hex
 
+    def test_check_max_media_upload_size(self):
+        try:
+            self.manager._check_max_media_upload_size()
+        except AssertionError:
+            pass
 
-def test_upload_media(manager: MatrixStickersManager):
-    # Image
-    manager._upload_media('cat.jpg')
+    def test_upload_media(self):
+        # Image
+        self.manager._upload_media('cat.jpg')
 
-    # Not image
-    try:
-        manager._upload_media('not_cat.txt')
-        raise Exception
-    except MatrixStickersManagerError:
-        pass
+        # Not image
+        try:
+            self.manager._upload_media('not_cat.txt')
+            raise Exception
+        except MatrixStickersManagerError:
+            pass
 
+    def test_get_room_state(self):
+        exists_event = self.manager._get_room_state(
+            pack_name=self.env_vars('TEST_EXISTS_PACK'),
+            room_id=self.env_vars('TEST_ROOM_ID')
+        )
 
-def test_get_room_state(manager: MatrixStickersManager, env_variables: Env):
-    event: dict = manager._get_room_state(pack_name=env_variables('TEST_EXISTS_PACK'),
-                                          room_id=env_variables('TEST_ROOM_ID'))
+        try:
+            missing_event = self.manager._get_room_state(
+                pack_name=uuid.uuid4().hex,
+                room_id=self.env_vars('TEST_ROOM_ID')
+            )
+            raise Exception
+        except MatrixStickersManagerError:
+            pass
 
+    def test_make_pack_obj(self):
+        # Exists pack
+        pack = self.manager._make_pack_obj(
+            name=self.env_vars('TEST_EXISTS_PACK'),
+            room_id=self.env_vars('TEST_ROOM_ID'),
+            new_if_missing=False
+        )
 
-def test_make_pack(manager: MatrixStickersManager, env_variables: Env):
-    # New pack
-    pack = manager._make_pack_obj(name=uuid.uuid4().hex)
-    # Existed pack
-    pack = manager._make_pack_obj(name=env_variables('TEST_EXISTS_PACK'), room_id=env_variables('TEST_ROOM_ID'))
+        # New pack
+        pack = self.manager._make_pack_obj(
+            name=uuid.uuid4().hex
+        )
 
+        # Missing packs
+        pack = self.manager._make_pack_obj(
+            name=uuid.uuid4().hex,
+            room_id=self.env_vars('TEST_ROOM_ID'),
+            new_if_missing=True
+        )
 
-def test_push_pack(manager: MatrixStickersManager, env_variables: Env):
-    pack = manager._make_pack_obj(name=env_variables('TEST_EXISTS_PACK'), room_id=env_variables('TEST_ROOM_ID'))
-    manager._push_pack(env_variables('TEST_EXISTS_PACK'), room_id=env_variables('TEST_ROOM_ID'), pack=pack)
+        try:
+            pack = self.manager._make_pack_obj(
+                name=uuid.uuid4().hex,
+                room_id=self.env_vars('TEST_ROOM_ID'),
+                new_if_missing=False
+            )
+            raise Exception
+        except (MatrixStickersManagerError, AssertionError):
+            pass
 
+    def test_push_pack(self):
+        self.manager._push_pack(
+            name=self.test_created_pack,
+            room_id=self.env_vars('TEST_ROOM_ID'),
+            pack=self.manager._make_pack_obj(name=self.test_created_pack)
+        )
 
-def test_load_stickers_from_folder(manager: MatrixStickersManager, env_variables: Env):
-    pack_name: str = uuid.uuid4().hex
+    def test_add_sticker_to_pack(self):
+        pack = self.manager._make_pack_obj(name=self.test_created_pack)
+        sticker_name = uuid.uuid4().hex
+        self.manager._add_sticker_to_pack(
+            pack=pack,
+            shortcode=sticker_name,
+            image_mxc=f'mxc://matrix.test.server/maeNguayielahseeci',
+            usage='sticker'
+        )
+        self.assertIn(sticker_name, pack['images'])
 
-    manager.load_pack_from_folder(
-        pack_name=pack_name,
-        folder_path='../stickers/',
-        room_id=env_variables('TEST_ROOM_ID')
-    )
-    manager.delete_pack(pack_name=pack_name, room_id=env_variables('TEST_ROOM_ID'))
+    def test_load_stickers_from_folder(self):
+        self.manager.load_pack_from_folder(
+            pack_name=self.test_created_pack,
+            folder_path='../stickers/',
+            room_id=self.env_vars('TEST_ROOM_ID')
+        )
 
+    def test_delete_pack(self):
+        self.manager.delete_pack(pack_name=self.test_created_pack, room_id=self.env_vars('TEST_ROOM_ID'))
 
-def test_is_server_admin(manager: MatrixStickersManager):
-    manager._is_server_admin()
+    def test_is_server_admin(self):
+        self.manager._is_server_admin()
 
+    def test_assemble_mxc_url(self):
+        assert ('test.matrix.server', 'tyBrcoBDixOSbEYwswVSwZer') == self.manager._assemble_mxc_url(
+            'mxc://test.matrix.server/tyBrcoBDixOSbEYwswVSwZer')
 
-def test_assemble_mxc_url(manager: MatrixStickersManager):
-    assert ('test.matrix.server', 'tyBrcoBDixOSbEYwswVSwZer') == manager._assemble_mxc_url(
-        'mxc://test.matrix.server/tyBrcoBDixOSbEYwswVSwZer')
-
-
-def test_export_pack(manager: MatrixStickersManager, env_variables: Env):
-    manager.export_pack(pack_name=env_variables('TEST_EXISTS_PACK'), room_id=env_variables('TEST_ROOM_ID'),
-                        export_folder='stickers_test/', original_name=True)
-    manager.export_pack(pack_name=env_variables('TEST_EXISTS_PACK'), room_id=env_variables('TEST_ROOM_ID'),
-                        export_folder='stickers_test/', original_name=False)
+    def test_export_pack(self):
+        self.manager.export_pack(pack_name=self.env_vars('TEST_EXISTS_PACK'), room_id=self.env_vars('TEST_ROOM_ID'),
+                                 export_folder='stickers_test/', original_name=True)
+        self.manager.export_pack(pack_name=self.env_vars('TEST_EXISTS_PACK'), room_id=self.env_vars('TEST_ROOM_ID'),
+                                 export_folder='stickers_test/', original_name=False)
